501eaab17fc926b64bf665148dce44a81eb7db8d
Alvin Zhu <alvinzhu33@yahoo.com>
2016-02-06 16:01:23 -0500

    Made better tests for negative numbers

--- 01recursion/Test.java
+++ 01recursion/Test.java
@@ -12,8 +12,20 @@ public class Test{
 	for(int i=0; i<a1.length; i++){
 	    System.out.println(a1[i]+": "+r1.sqrt(a1[i]));
 	}
-	System.out.println(r1.sqrt(-1));
-	System.out.println(r1.sqrt(-1));
-	System.out.println(r1.sqrt(-1));
+	try{
+	    System.out.println(r1.sqrt(-1));
+	}catch(IllegalArgumentException e){
+	    System.out.println("-1 can't be squarerooted");
+	}
+	try{
+	    System.out.println(r1.sqrt(-2));
+	}catch(IllegalArgumentException e){
+	    System.out.println("-2 can't be squarerooted");
+	}
+	try{
+	    System.out.println(r1.sqrt(-100));
+	}catch(IllegalArgumentException e){
+	    System.out.println("-100 can't be squarerooted");
+	}
     }
 }
