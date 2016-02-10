80020b982b20f2514ba2ff9695b0d7a74f4036d7
Alvin Zhu <alvinzhu33@yahoo.com>
2016-02-09 22:09:28 -0500

    Added the add Queen and subtract Queen functions

--- 02NQueens/Board.java
+++ 02NQueens/Board.java
@@ -12,6 +12,41 @@ public class Board{
 	}
     }
 
+    public boolean addQ(int row, int col){
+	if(board[row][col]==0){
+	    board[row][col]=1;
+	    Qterritory(row, col, 1);
+	    return true;
+	}else{
+	    return false;
+	}
+    }
+
+    public boolean minusQ(int row, int col){
+	if(board[row][col]==1){
+	    board[row][col]=0;
+	    Qterritory(row, col, -1);
+	    return true;
+	}
+	return false;
+    }
+
+    public void Qterritory(int row, int col, int change){
+	int c=col+1;
+	for(int r=row-1; r>0; row--){
+	    board[r][c]+=change;
+	    c++;
+	}
+	int c=col+1;
+	for(int r=row+1; r<board.length; r++){
+	    board[r][c]+=change;
+	    c++;
+	}
+	for(int c=col+1; c<board[0].length; c++){
+	    board[r][c]+=change;
+	}
+    }
+
     public void solve(){
 	System.out.println(Arrays.toString(board));
     }
