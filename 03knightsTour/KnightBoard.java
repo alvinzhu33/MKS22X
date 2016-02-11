61c583c3fb2b5b8905995b0b56b055fa36e1ef51
Alvin Zhu <alvinzhu33@yahoo.com>
2016-02-11 12:15:15 -0500

    Set up the KnightBoard.java file

---
+++ 03knightsTour/KnightBoard.java
@@ -0,0 +1,17 @@
+public class KnightBoard{
+    private static boolean DEBUG=true;
+
+    private boolean solve(){
+	
+    }
+
+    private void printSolution(){
+	
+    }
+
+    private void debug(String s){
+	if(DEBUG){
+	    System.out.println(s);
+	}
+    }
+}
