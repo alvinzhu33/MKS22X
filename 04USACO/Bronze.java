20595c92fe9a00ba1b27472cd8b5a22f941d661d
Alvin Zhu <alvinzhu33@yahoo.com>
2016-02-22 12:33:40 -0500

    Made files

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -1,3 +1,13 @@
-public class Bronze{
+import java.util.Scanner;
+import java.io.*;
 
+public class Bronze{
+    public static void main(String[]args) throws IOException{
+	File read = new File("makelake.txt");
+	Scanner scan = new Scanner(read);
+	while(scan.hasNextLine()){
+	    System.out.println(scan.nextLine());
+	}
+	scan.close();
+    }
 }
