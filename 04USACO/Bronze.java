24faa01d7324165dcec175323779d8b4da70cbae
Alvin Zhu <alvinzhu33@yahoo.com>
2016-02-22 19:37:56 -0500

    Successfully scanned input file

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -3,33 +3,50 @@ import java.io.*;
 
 public class Bronze{
     int[][]board;
-    int row;
-    int col;
     int elevation;
-    int numOper;
+    int[][]operations;
 
     public Bronze(){
 	importFile();
+	/*print(board);
+	print(operations);
+	System.out.println(elevation);*/
+    }
+
+    public void print(int[][] a1){
+	String print="";
+	for(int r=0; r<a1.length; r++){
+	    for(int c=0; c<a1[r].length; c++){
+		if(a1[r][c]<10){
+		    print+=" ";
+		}
+		print+=a1[r][c]+" ";
+	    }
+	    print+="\n";
+	}
+	System.out.println(print);
     }
     
     public void importFile(){
 	File read = new File("makelake.txt");
 	try{
 	    Scanner scan = new Scanner(read);
-	    row = scan.nextInt();
-	    col = scan.nextInt();
+	    int row = scan.nextInt();
+	    int col = scan.nextInt();
 	    elevation = scan.nextInt();
-	    numOper = scan.nextInt();
-
-	    System.out.println(row);
-	    System.out.println(col);
-	    System.out.println(elevation);
-	    System.out.println(numOper);
+	    int opers = scan.nextInt();
 
 	    board = new int[row][col];
 	    for(int r=0; r<row; r++){
 		for(int c=0; c<col; c++){
 		    board[r][c]=scan.nextInt();
+		}
+	    }
+
+	    operations = new int[opers][3];
+	    for(int o=0; o<opers; o++){
+		for(int n=0; n<3; n++){
+		    operations[o][n]=scan.nextInt();
 		}
 	    }
 
