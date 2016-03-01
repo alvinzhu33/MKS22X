c98f449291e9c049dfca55e11a1005d483de298e
Alvin Zhu <alvinzhu33@yahoo.com>
2016-03-01 18:15:59 -0500

    Working function :D

--- 04USACO/Silver.java
+++ 04USACO/Silver.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 import java.io.*;
 
 public class Silver{
     int[][]board;
+    int[][]copy;
     int steps;
     int startr,startc,endr,endc;
 
     public Silver(){
-	File read = new File("ctravel1.in");
+	File read = new File("ctravel2.in");
 	try{
 	    Scanner scan = new Scanner(read);
 	    int row = scan.nextInt();
@@ -15,6 +16,7 @@ public class Silver{
 	    steps = scan.nextInt();
 	    
 	    board = new int[row][col];
+	    copy = new int[row][col];
 	    for(int r=0; r<row; r++){
 		String line = scan.next();
 		for(int c=0; c<col; c++){
@@ -32,31 +34,29 @@ public class Silver{
 	    endc=scan.nextInt()-1;
 	    
 	    scan.close();
+	    makeCopy();
 	}catch(FileNotFoundException e){
 	    System.out.println("No such file");
 	}
     }
 
+    public void makeCopy(){
+	for(int r=0; r<board.length; r++){
+	    for(int c=0; c<board[r].length; c++){
+		copy[r][c]=board[r][c];
+	    }
+	}
+    }
+
     public boolean solve(){
 	start();
-	for(int step=1; step<steps; step++){
+	for(int step=1; step<steps+1; step++){
 	    for(int r=0; r<board.length; r++){
-		if((startc%2==1 && startc%2==1)||(startr%2==0 && startc==0)){
-		    for(int c=0; c<board[r].length; c+=2){
-			change(r,c);
-		    }
-		    for(int c=1; c<board[r].length; c+=2){
-			change(r,c);
-		    }
-		}else{
-		    for(int c=1; c<board[r].length; c+=2){
-			change(r,c);
-		    }
-		    for(int c=0; c<board[r].length; c+=2){
-			change(r,c);
-		    }
+		for(int c=0; c<board[r].length; c++){
+		    change(r,c);
 		}
 	    }
+	    makeCopy();
 	}
 	System.out.println(board[endr][endc]+",6,Zhu,Alvin");
 	return true;
@@ -78,23 +78,20 @@ public class Silver{
     }
 
     public void change(int row, int col){
-	if(board[row][col]>0){
-	    board[row][col]=0;
-	}else{
-	    if(board[row][col]!=-1){
-		if(row-1>0 && board[row-1][col]!=-1){
-		    board[row][col]+=board[row-1][col];
-		}
-		if(row+1<board.length && board[row+1][col]!=-1){
-		    board[row][col]+=board[row+1][col];
-		}
-		if(col-1>0 && board[row][col-1]!=-1){
-		    board[row][col]+=board[row][col-1];
-		}
-		if(col+1<board[0].length && board[row][col+1]!=-1){
-		    board[row][col]+=board[row][col+1];
-		}
+	if(board[row][col]>0 && copy[row][col]>0){
+	    if(row-1>=0 && board[row-1][col]!=-1){
+		board[row-1][col]+=board[row][col];
 	    }
+	    if(row+1<board.length && board[row+1][col]!=-1){
+		board[row+1][col]+=board[row][col];
+	    }
+	    if(col-1>=0 && board[row][col-1]!=-1){
+		board[row][col-1]+=board[row][col];
+		}
+	    if(col+1<board[0].length && board[row][col+1]!=-1){
+		board[row][col+1]+=board[row][col];
+	    }
+	    board[row][col]=0;
 	}
     }
     
@@ -105,7 +102,7 @@ public class Silver{
 		if(board[r][c]<10 && board[r][c]>=0){
 		    print+=" ";
 		}
-		print+=board[r][c]+" ";
+		print+=board[r][c]+"\t";
 	    }
 	    print+="\n";
 	}
@@ -114,8 +111,6 @@ public class Silver{
 
     public static void main(String[]args){
 	Silver s1 = new Silver();
-	s1.print();
 	s1.solve();
-	s1.print();
     }
 }
