ffd09583c276b4b80eb0f246f7a37408991072f4
Alvin Zhu <alvinzhu33@yahoo.com>
2016-03-03 12:13:13 -0500

    ACTUALLY copied it over now

--- 06merge/Sorts.java
+++ 06merge/Sorts.java
@@ -1,101 +1,171 @@
+import java.util.*;
+
 public class Sorts{
     public static void printArray(int[]data){
-      String show="[ "+data[0];
-      for(int i=1; i<data.length; i++){
-         show+=", "+data[i];
-      }
-      show+="]";
-      System.out.println(show);
-   }
-
-   public static int getIndex(int[] data, int value){
-      int i=1;
-      if(value<data[0]){
-         return 0;
-      }
-      while(value>data[i] && i<data.length && data[i]!=0){
-         i+=1;
-      }
-      return i;
-   }
-
-   public static int[] add(int[] data, int value){
-      int index= getIndex(data, value);
-      int[] copy = new int[data.length];
-      if(index==data.length){
-         for(int i=0; i<data.length; i++){
-            copy[i]=data[i];
-         }
-         copy[copy.length-1]=value;
-      }
-      for(int i=0; i<data.length; i++){
-         if(i<index){
-            copy[i]=data[i];
-         }
-         if(i==index){
-            copy[i]=value;
-         }
-         if(i>index){
-            copy[i]=data[i-1];
-         }
-      }
-      return copy;
-   }
-
-    //My Code
-    
-    /*public static void insertionSort(int[]data){
-      int[] copy= new int[data.length];
-      copy[0]=data[0];
-      if(data[1]<=copy[0]){
-         copy[1]=copy[0];
-         copy[0]=data[1];
-      }
-      else{
-         copy[1]=data[1];
-      }
-      for(int i=2; i<data.length; i++){
-         copy=add(copy, data[i]);
-      }
-      for(int i=0; i<copy.length; i++){
-         data[i]=copy[i];
-      }
-      //your code here to make data re-order its elements
-      //from least to greatest just like we did in class
-      //same algorithm as the way you created your OrderedSuperArray
-      }*/
-
-    /*public static void fillRandom(int[] data){
-	for(int i=0; i<data.length; i++){
-	    int sign=(int)Math.pow(-1, (int)(Math.random()*2));
-	    data[i]= sign*(int)(Math.random()*Integer.MAX_VALUE);
-	    //Integer is a class
-	    //MAX_VALUE is a public, static, and final variable
-	}
-	}*/
-
-    //------------------------------------------------------------
-    //InsertionSort
-    public static void insertionSort(int[] data){
+	String show="[ "+data[0];
 	for(int i=1; i<data.length; i++){
-	    int current=data[i];
-	    int j =i;
-	    while(j>0 && data[j-1]>current){
-		data[j]=data[j-1];
-		j--;
+	    show+=", "+data[i];
+	}
+	show+="]";
+	System.out.println(show);
+	//print the array like:  [ 1, 2, 3, 4]
+    }
+
+    public static int getIndex(int[] data, int value){
+	int i=1;
+	if(value<data[0]){
+	    return 0;
+	}
+	/*for(; i<data.length;){
+	  while(value>data[i]){
+	  i+=1;
+	  }
+	  }*/
+	while(value>data[i] && i<data.length && data[i]!=0){
+	    i+=1;
+	}
+	return i;
+    }
+
+    public static int[] add(int[] data, int value){
+	int index= getIndex(data, value);
+	int[] copy = new int[data.length];
+	if(index==data.length){
+	    for(int i=0; i<data.length; i++){
+		copy[i]=data[i];
 	    }
-	    data[j]=current;
+	    copy[copy.length-1]=value;
+	}
+	for(int i=0; i<data.length; i++){
+	    if(i<index){
+		copy[i]=data[i];
+	    }
+	    if(i==index){
+		copy[i]=value;
+	    }
+	    if(i>index){
+		copy[i]=data[i-1];
+	    }
+	}
+	return copy;
+    }
+    
+    //----------------------------------------------------------------
+    //Insertion Sort
+    public static void insertionSort(int[]data){
+	for(int index=1; index<data.length; index++){
+	    int value=data[index];
+	    int swapIndex =index;
+	    while(swapIndex>0 && data[swapIndex-1]>value){
+		data[swapIndex]=data[swapIndex-1];
+		swapIndex--;
+	    }
+	    data[swapIndex]=value;
+	    //printArray(data);
+	}
+    }
+    
+    public static void fillRandom(int[] data){
+	for(int x=0; x<data.length; x++){
+	    int pm = (int)Math.pow(-1,(int)Math.random()*2);
+	    data[x]= pm*(int)(Math.random()*Integer.MAX_VALUE);
 	}
     }
 
-
-    //---------------------------------------------------------------
-    //SelectionSort
+    //------------------------------------------------------------------
+    //Selection Sort
     public static void selectionSort(int[] data){
-	int[] newSorted = new int[data.length];
-	for
+	for(int index=0; index<data.length; index++){
+	    int next=data[index];
+	    int storeValue=data[index];
+	    int storeIndex=-1;
+	    for(int searcher=index; searcher<data.length; searcher++){
+		if(data[searcher]<=next){
+		    next=data[searcher];
+		    storeIndex=searcher;
+		}
+	    }
+	    data[index]=next;
+	    data[storeIndex]=storeValue;
+	    //printArray(data);
+	}
+    }
+
+    //---------------------------------------------------------------------
+    //Bubble Sort
+    public static void bubbleSort(int[]data){
+	for(int place=1; place<data.length; place++){
+	    int index=1;
+	    while(index+place<=data.length){
+		int storeValue=data[index-1];
+		if(data[index-1]>data[index]){
+		    data[index-1]=data[index];
+		    data[index]=storeValue;
+		}
+		index++;
+	    }
+	    //printArray(data);
+	}
     }
 
     //----------------------------------------------------------------
-    //MergeSort
+    //Merge Sort
+    public static int[] easymerge(int[]aryA, int[]aryB){
+	int[] sort = new int[aryA.length+aryB.length];
+	int aPlace=0;
+	int bPlace=0;
+	int sortPlace=0;
+	while(aPlace<aryA.length && bPlace<aryB.length){
+	    if(aryA[aPlace]<=aryB[bPlace]){
+		sort[sortPlace]=aryA[aPlace];
+		aPlace++;
+		sortPlace++;
+	    }else{
+		sort[sortPlace]=aryB[bPlace];
+		bPlace++;
+		sortPlace++;
+	    }
+	}
+	for(int a=aPlace; a<aryA.length; a++){
+	    sort[sortPlace]=aryA[a];
+	    sortPlace++;
+	}
+	for(int b=bPlace; b<aryB.length; b++){
+	    sort[sortPlace]=aryB[b];
+	    sortPlace++;
+	}
+	return sort;
+    }
+
+    public static void merge(int[]data, 
+			     int startA, int endA, 
+			     int startB, int endB){
+	int[] copy = new int[(endA-startA)+1+(endB-startB)+1];
+
+	int aPlace=startA;
+	int bPlace=startB;
+	int sortPlace=0;
+
+	while(aPlace<=endA && bPlace<=endB){
+	    if(data[aPlace]<=data[bPlace]){
+		copy[sortPlace]=data[aPlace];
+		aPlace++;
+		sortPlace++;
+	    }else{
+		copy[sortPlace]=data[bPlace];
+		bPlace++;
+		sortPlace++;
+	    }
+	}
+	
+	for(int a=aPlace; a<=endA; a++){
+	    copy[sortPlace]=data[a];
+	    sortPlace++;
+	}
+	for(int b=bPlace; b<=endB; b++){
+	    copy[sortPlace]=data[b];
+	    sortPlace++;
+	}
+	System.out.println(Arrays.toString(copy));
+    }
 }
