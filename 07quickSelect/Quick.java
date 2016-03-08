52293cd7ae5c04e85a449c3610550f187c992065
Alvin Zhu <alvinzhu33@yahoo.com>
2016-03-07 20:08:17 -0500

    Created the file

---
+++ 07quickSelect/Quick.java
@@ -0,0 +1,27 @@
+public class Quick{
+    
+    //choose a random partition element at a location from left to right inclusive.
+    //partition the array such that the randomly chosen element divides all values smaller and larger than it. 
+    //smaller elements on the left side of the partition, larger on the right side
+    //return the index of the partition element. 
+    // this allows your quickselect method to decide where to go next.
+    private static int partition(int[]data, int left, int right){
+
+    }
+    
+    //return the kth smallest value.
+    // when k = 0 return the smallest.
+    // 0 <= k < data.length
+    public static int quickselect(int[]data, int k){
+
+    }
+    
+    //return the kth smallest value in the given left/right range 
+    // left <= k <= right
+    //start by calling the helper as follows:
+    // quickselect(data,k,0,data.length-1)
+    private static int quickselect(int[]data, int k, int left, int right){
+	
+    }
+
+}
