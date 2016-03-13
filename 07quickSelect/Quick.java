817d85bdc507ea999dd09f683cdbabd6a9febbe3
Alvin Zhu <alvinzhu33@yahoo.com>
2016-03-13 00:03:16 -0500

    Tried coming up with new algorithm but failed

--- 07quickSelect/Quick.java
+++ 07quickSelect/Quick.java
@@ -119,6 +119,40 @@ public class Quick{
 
       //System.out.println(Arrays.toString(data));
       return returning;
+
+	/*int[] copy = new int[data.length];
+	int compare = data[(int)(Math.random()*(right-left+1))+left];
+	//System.out.println(compare);
+
+	int from = left;
+	int to = right;
+	int pivotCounter = 0;
+	for(int i=from; i<copy.length && to>0; i++){
+	    if(data[i]<compare){
+		copy[from]=data[i];
+		from++;
+	    }
+	    if(data[i]==compare){
+		pivotCounter++;
+	    }
+	    if(data[i]>compare){
+		copy[to]=data[i];
+		to--;
+	    }
+	}
+
+	int[] answer = new int[2];
+	answer[0]=from;
+	for(int i=0; i<pivotCounter; i++){
+	    copy[from+i]=compare;
+	    answer[1]=from+i;
+	}
+
+	for(int i=left; i<=right; i++){
+	    data[i]=copy[i];
+	}
+	System.out.println(Arrays.toString(data));
+	return answer;*/
    }
 
     public static void quickSort(int[]data){
@@ -127,13 +161,11 @@ public class Quick{
     
     private static void quickSort(int[]data,int left,int right){
 	if(right-left+1>1){
-	    int to=partition(data,left,right)[0]-1;
-	    int from=partition(data,left,right)[1]+1;
-
-	    System.out.println(to);
+	    int[] answers=partition(data,left,right);
+	    int to=answers[0];
+	    int from=answers[1];
+	    
 	    quickSort(data,left,to);
-
-	    System.out.println(from);
 	    quickSort(data,from,right);
 	}
     }
@@ -156,7 +188,7 @@ public class Quick{
 	System.out.println(Arrays.toString(partition(e,0,e.length-1)));*/
 	quickSort(a);
 	System.out.println(Arrays.toString(a));
-	System.out.println("-----------------");
+	System.out.println("-----------------");/*
 	quickSort(b);
 	System.out.println(Arrays.toString(b));
 	System.out.println("-----------------");
@@ -168,6 +200,6 @@ public class Quick{
 	System.out.println("-----------------");
 	quickSort(e);
 	System.out.println(Arrays.toString(e));
-	System.out.println("-----------------");
+	System.out.println("-----------------");*/
     }
 }
