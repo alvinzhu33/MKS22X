d563e6899129819cf5b87aebf79f76f493b27097
Alvin Zhu <alvinzhu33@yahoo.com>
2016-03-15 20:35:48 -0400

    Finally made a working add and toString!

--- 08MyLinkedList/Driver.java
+++ 08MyLinkedList/Driver.java
@@ -3,6 +3,10 @@ public class Driver{
 	MyLinkedList l1 = new MyLinkedList();
 
 	l1.add(5);
+	l1.add(4);
+	l1.add(6);
+	l1.add(7);
+	l1.add(8);
 	System.out.println(l1);
     }
 }
--- 08MyLinkedList/MyLinkedList.java
+++ 08MyLinkedList/MyLinkedList.java
@@ -4,14 +4,14 @@ public class MyLinkedList{
 
     public MyLinkedList(){
 	size=0;
-	LNode start = new LNode();
+	start = new LNode();
     }
 
     //get the value of the element at the specified index (0 based)
     public int get(int index){
 	return 1;
     }
-    
+
     //change the value of the element at the specified index to the newValue, return the old value
     public int set(int index,int newValue){
 	return 1;
@@ -35,10 +35,15 @@ public class MyLinkedList{
     //adds to end
     public boolean add(int value){
 	LNode current = start;
-	if(current.getNext()==null){
-	    current.set(value);
+	while(current.getNext()!=null){
+	    current = current.getNext();
 	}
-	return false;
+	current.set(value);
+
+	LNode next = new LNode();
+	size++;
+	current.setNext(next);
+	return true;
     }
 
     //returns the index of the 1st occurrence of the value in the linked list, -1 if not found.
@@ -48,11 +53,16 @@ public class MyLinkedList{
 
     //returns a list formatted like: [ v1, v2, v3, ... vn-1, vn ]
     public String toString(){
-	String copy="[";
+	String copy="[ "+start.get();
 
 	LNode current = start;
-	while(current.getNext()!=null){
-	    copy += " "+current.get();
+	/*while(current.getNext()!=null){
+	  current = current.getNext();
+	  copy += ", "+current.get();
+	  }*/
+	for(int i=0; i<size-1; i++){
+	    current = current.getNext();
+	    copy += ", "+current.get();
 	}
 	copy += "]";
 
@@ -63,11 +73,15 @@ public class MyLinkedList{
     public class LNode{
 	public int data;
 	public LNode next;
-	
-	public LNode(int car){
-	    data=car;
+
+	public LNode(){
+	    data=0;
 	}
-	
+
+	/*public LNode(int car){
+	  data=car;
+	  }*/
+
 	public LNode(int car, LNode cdr){
 	    data = car;
 	    next = cdr;
@@ -83,12 +97,12 @@ public class MyLinkedList{
 
 	public boolean set(int value){
 	    data = value;
-	    return false;
+	    return true;
 	}
 
 	public boolean setNext(LNode cdr){
 	    next = cdr;
-	    return false;
+	    return true;
 	}
     }
 }
