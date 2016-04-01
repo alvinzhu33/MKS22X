b8ccee3a085a5ce5f4da696db049788f361eaf77
Alvin Zhu <alvinzhu33@yahoo.com>
2016-04-01 12:29:17 -0400

    Made array of objects, started grow

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -1,18 +1,50 @@
 import java.util.*;
 
 public class MyDeque<T>{
+    Object[] data;
+    int start,end;
+
+    public MyDeque(){
+	data = new Object[10];
+    }
+
     //You need a private method to grow the array and copy over the values.
-    
+    private void grow(){
+	Object[] copy = new Object[data.length*2];
+	
+	for(int i=0; i<data.length; i++){
+	    copy[i]=data[i];
+	}
+
+	data = copy;
+    }
+
     //When the array is full, re-size, then add. 
     //No exceptions are required since you will re-size.
-    public void addFirst(T value);
-    public void addLast(T value);
+    public void addFirst(T value){
+	
+    };
+    public void addLast(T value){
+	
+    };
     
     //NoSuchElementException is thrown when there are no elements. 
-    public T removeFirst();  
-    public T removeLast();  
+    public T removeFirst(){
+	return end;
+    };  
+    public T removeLast(){
+	return start;
+    };  
     
     //NoSuchElementException is thrown when there are no elements. 
-    public T getFirst();
-    public T getLast();
+    public T getFirst(){
+	return start;
+    };
+    public T getLast(){
+	return end;
+    };
+
+    public String toString(){
+	return "hello";
+    };
 }
