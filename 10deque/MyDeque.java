a26ac7bf8d1c1f56083d078c90ba065c843abb27
Alvin Zhu <alvinzhu33@yahoo.com>
2016-04-02 21:04:39 -0400

    No more unchecked warning!

--- 10deque/MyDeque.java
+++ 10deque/MyDeque.java
@@ -1,78 +1,92 @@
 import java.util.*;
 
 public class MyDeque<T>{
-  Object[] data;
-  int start,end;
-  int size=0;
+    T[] data;
+    int start,end;
+    int size=0;
 
-  public MyDeque(){
-    data = new Object[10];
-  }
-
-  //You need a private method to grow the array and copy over the values.
-  private void grow(){
-    Object[] copy = new Object[data.length*2];
-
-    for(int i=0; i<data.length; i++){
-      copy[i]=data[i];
+    @SuppressWarnings("unchecked")
+    public MyDeque(){
+        data = (T[]) new Object[10];
     }
 
-    data = copy;
-  }
+    @SuppressWarnings("unchecked")
+    //You need a private method to grow the array and copy over the values.
+    private void grow(){
+        T[] copy = (T[]) new Object[data.length*2];
 
-  //When the array is full, re-size, then add.
-  //No exceptions are required since you will re-size.
-  public void addFirst(T value){
-    if(size==data.length){
-      grow();
-    }
-    if(start==0 && data[start]!=null){
-      start=data.length-1;
-    }else{
-      start--;
-    }
-    data[start]=value;
-    size++;
-  };
-  public void addLast(T value){
-    if(size==data.length){
-      grow();
-    }
-    if(end==data.length-1){
-      end=0;
-    }else{
-      end++;
-    }
-    data[end]=value;
-    size++;
-  }
+        for(int i=0; i<data.length; i++){
+            copy[i]=data[i];
+        }
 
-  //NoSuchElementException is thrown when there are no elements.
-  public T removeFirst(){
-    return (T)data[start];
-  }
-  public T removeLast(){
-    return (T)data[end];
-  }
-
-  //NoSuchElementException is thrown when there are no elements.
-  public T getFirst(){
-    return (T)data[start];
-  }
-  public T getLast(){
-    return (T)data[end];
-  }
-
-  public String toString(){
-    String ans="[ ";
-    for(int i=0; i<data.length-1; i++){
-      ans+=data[i]+", ";
+        data = copy;
     }
-    ans+=data[data.length-1]+"]";
-    return ans;
-  }
 
-  public static void main(String[]args){
-    MyDeque<Integer> t = new MyDeque<Integer>();
-  }
+    //When the array is full, re-size, then add.
+    //No exceptions are required since you will re-size.
+    public void addFirst(T value){
+        if(size==data.length){
+            grow();
+        }
+        if(start==0){
+            if(data[start]==null){
+                start=0;
+            }else{
+                start=data.length-1;
+            }
+        }else{
+            start--;
+        }
+        data[start]=value;
+        size++;
+    }
+    public void addLast(T value){
+        if(size==data.length){
+            grow();
+        }
+        if(end==data.length-1 || data[end]==null){
+            end=0;
+        }else{
+            end++;
+        }
+        data[end]=value;
+        size++;
+    }
+
+    //NoSuchElementException is thrown when there are no elements.
+    public T removeFirst(){
+        return data[end];
+    }
+    public T removeLast(){
+        return data[end];
+    }
+
+    //NoSuchElementException is thrown when there are no elements.
+    public T getFirst(){
+        return data[start];
+    }
+    public T getLast(){
+        return data[end];
+    }
+
+    public String toString(){
+        String ans="[ ";
+        for(int i=0; i<data.length-1; i++){
+            ans+=data[i]+", ";
+        }
+        ans+=data[data.length-1]+"]";
+        return ans;
+    }
+
+    public static void main(String[]args){
+        MyDeque<Integer> t = new MyDeque<Integer>();
+        t.addFirst(0);
+        t.addFirst(-1);
+        t.addFirst(-2);
+        t.addLast(1);
+        t.addLast(2);
+        System.out.println(t);
+        System.out.println(t.getFirst());
+        System.out.println(t.getLast());
+    }
 }
