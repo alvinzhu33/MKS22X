a1dc6633d64e74a60234ed040c1f72d2ef37f0c7
Alvin Zhu <alvinzhu33@yahoo.com>
2016-04-23 23:21:02 -0400

    Working add and toString

--- 12BSTree/BSTree.java
+++ 12BSTree/BSTree.java
@@ -2,112 +2,114 @@ import java.util.*;
 
 public class BSTree<T extends Comparable<T>>{
     private class Node{
-	T data;
-	Node left;
-	Node right;
+        T data;
+        Node left;
+        Node right;
 
-	public T getData(){
-	    return data;
-	}
-	public Node getLeft(){
-	    return left;
-	}
-	public Node getRight(){
-	    return right;
-	}
+        public Node(){
+            data=null;
+        }
 
-	public void setData(T value){
-	    data=value;
-	}
-	public void setLeft(Node l){
-	    left=l;
-	}
-	public void setRight(Node r){
-	    right=r;
-	}
+        public Node(T value){
+            data=value;
+        }
 
-	public int height(){ 
-	    if(left!=null || right!=null){
-		return 1;
-	    }
-	    return 0;
-	}
-	public void add(T value){
-	    /*if(value.compareTo(data)<0){
-		left = new Node(value);
-	    }
-	    if(value.compareTo(data)>0){
-		right = new Node(value);
-	    }
+        public T getData(){
+            return data;
+        }
+        public Node getLeft(){
+            return left;
+        }
+        public Node getRight(){
+            return right;
+        }
 
-	    Node check=root;
-	    Node added=root;
-	    while(check.height()!=0){
-		if(value.compareTo(check.getData())<0){
-		    check=check.getLeft(); 
-		}else{
-		    check=check.getRight();
-		}
-	    }
-	    if(value.compareTo(check.getData())<0){
-		check.setLeft(new Node(value));
-	    }else{
-		check.setRight(new Node(value));
-	    }
-	    root=check;
-	    System.out.println(root);*/
-	    if(height()==0){
-		data=value;
-	    }
-	}
-	public String toString(){
-	    String l = "_";
-	    String r = "_";
-	    if(left!=null){
-		l=""+left;
-	    }
-	    if(right!=null){
-		r=""+right;
-	    }
-	    return ""+data+":| "+l+" "+r+"| ";
-	}
-	public boolean contains(T value){
-	    return data==value;
-	}
-     
+        public void setData(T value){
+            data=value;
+        }
+        public void setLeft(Node l){
+            left=l;
+        }
+        public void setRight(Node r){
+            right=r;
+        }
+
+        public int height(){
+            int counter=0;
+            if(left!=null || right!=null){
+                return 1;
+            }
+            return 0;
+        }
+        public void add(T value){
+            if(data==null){
+                data=value;
+            }else{
+                if(value.compareTo(data)<0){
+                    if(left==null){
+                        setLeft(new Node(value));
+                    }else{
+                        left.add(value);
+                    }
+                }else{
+                    if(right==null){
+                        setRight(new Node(value));
+                    }else{
+                        right.add(value);
+                    }
+                }
+            }
+        }
+        public String toString(){
+            String l = "_";
+            String r = "_";
+            if(left!=null){
+                l=""+left.toString();
+            }
+            if(right!=null){
+                r=""+right.toString();
+            }
+            return "|"+data+": "+l+" "+r+"| ";
+        }
+        public boolean contains(T value){
+            return data==value;
+        }
+
     }
 
     private Node root;
 
     public BSTree(){
-	root=new Node();
+        root=new Node();
     }
 
     //OUTER methods here are wrapper methods for the root
     public int getHeight(){
-	//call the root's methods
-	//check for empty first!
-	return root.height();
+        //call the root's methods
+        //check for empty first!
+        return root.height();
     }
 
     public void add(T value){
-	root.add(value);
+        root.add(value);
     }
     public String toString(){
-	//check for empty before you do things with root.
-	return root.toString();
+        //check for empty before you do things with root.
+        return root.toString();
     }
     public boolean contains(T value){
-	//check for empty before you do things with root.
-	return false;
+        //check for empty before you do things with root.
+        return false;
     }
 
     public static void main(String[]args){
-	BSTree<Integer> b= new BSTree<Integer>();
-	b.add(1);
-	System.out.println(b);
-	//b.add(0);
-	//b.add(5);
-	//b.add(-1);
+        BSTree<Integer> b= new BSTree<Integer>();
+        b.add(1);
+        b.add(0);
+        b.add(5);
+        b.add(-1);
+        b.add(6);
+        b.add(4);
+        System.out.println(b);
     }
 }
