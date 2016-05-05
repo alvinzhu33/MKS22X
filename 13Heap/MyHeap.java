1f15088e312a0986f43dca5cd422bfc84dc5d3af
Alvin Zhu <alvinzhu33@yahoo.com>
2016-05-05 12:31:52 -0400

    Started instantiating

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,5 +1,5 @@
 import java.util.*;
-@SuppressWarnings("unchecked");
+@SuppressWarnings("unchecked")
 
 public class MyHeap<T extends Comparable<T>>{
     private int size;
@@ -7,13 +7,19 @@ public class MyHeap<T extends Comparable<T>>{
     private boolean isMax=true;
 
     public MyHeap(){
-	
+	data = (T[]) new Object[9];
+	size = 0;
     }
     public MyHeap(T[] array){
-	
+	size = array.length+1;
+	data = (T[]) new Object[size+1];
+	for(int i=1; i<size-1; i++){
+	    data = array[i];
+	}
+	heapify();
     }
     private void pushDown(int k){
-	
+
     }
     private void pushUp(int k){
 	
@@ -22,16 +28,19 @@ public class MyHeap<T extends Comparable<T>>{
 	
     }
     public T delete(){
-	
+	return data[0];
     }
     public void add(T x){
-	
+	if(size==0){
+	    data[1]=x;
+	}
+	size++;
     }
     private void doubleSize(){
 	
     }
     public String toString(){
-	
+	return "";
     }
 
     public MyHeap(boolean isMax){
