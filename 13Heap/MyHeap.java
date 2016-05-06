524bc9fec0c83fbdc0d581f0f8b42cc36384f077
Alvin Zhu <alvinzhu33@yahoo.com>
2016-05-06 12:31:41 -0400

    did stuff

--- 13Heap/MyHeap.java
+++ 13Heap/MyHeap.java
@@ -1,31 +1,52 @@
 import java.util.*;
 @SuppressWarnings("unchecked")
-
 public class MyHeap<T extends Comparable<T>>{
     private int size;
     private T[] data;
     private boolean isMax=true;
 
     public MyHeap(){
-	data = (T[]) new Object[9];
+	data = (T[]) new Comparable[9];
 	size = 0;
     }
     public MyHeap(T[] array){
 	size = array.length+1;
-	data = (T[]) new Object[size+1];
+	data = (T[]) new Comparable[size+1];
 	for(int i=1; i<size-1; i++){
-	    data = array[i];
+	    data[i] = array[i];
 	}
 	heapify();
     }
-    private void pushDown(int k){
+    public MyHeap(boolean isMax){
+	data = (T[]) new Comparable[9];
+	size = 0;
+	this.isMax = isMax;
+    }
+    public MyHeap(T[] array, boolean isMax){
+	
+    }
 
+    private void pushDown(int k){
+	T store = data[k];
+	System.out.println(k);
+	System.out.println(store);
     }
     private void pushUp(int k){
-	
+	T store = data[k];
+	System.out.println(k);
+	System.out.println(store);
     }
     private void heapify(){
-	
+	int child = size;
+	for(int i=size/2; i>0; i--){
+	    if(isMax && data[i].compareTo(data[child])<0){
+		pushDown(i);
+	    }
+	    if(!isMax && data[i].compareTo(data[child])>0){
+		pushUp(i);
+	    }
+	    child--;
+	}
     }
     public T delete(){
 	return data[0];
@@ -33,20 +54,31 @@ public class MyHeap<T extends Comparable<T>>{
     public void add(T x){
 	if(size==0){
 	    data[1]=x;
+	}else{
+	    if(size+1>=data.length){
+		doubleSize();
+	    }
+	    data[size+1]=x;
 	}
 	size++;
+	heapify();
     }
     private void doubleSize(){
-	
+	T[] doubled = (T[]) new Object[(data.length-1)*2+1];
     }
     public String toString(){
-	return "";
+	String ans="";
+	for(int i=1; i<=size; i++){
+	    ans+=data[i]+" ";
+	}
+	return ans;
     }
 
-    public MyHeap(boolean isMax){
-	
-    }
-    public MyHeap(T[] array, boolean isMax){
-	
+    public static void main(String[]args){
+	MyHeap<Integer> a = new MyHeap<>(false);
+	a.add(5);
+	a.add(4);
+	a.add(3);
+	System.out.println(a);
     }
 }
